60eeb9ed8386e7f8644f6729525203451814424a
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-12-30 09:52:54 +0100

    .

--- runme.sh
+++ runme.sh
@@ -1,6 +1,9 @@
+BIBINPUTS=":.:/home/vruiz/bibs/:"
 make clean
-grep citation ~/LZ77/index.aux >> index.aux
+grep citation ~/Run-length_encoding/index.aux >> index.aux
 grep citation ~/Burrows-Wheeler_transform/index.aux >> index.aux
+grep citation ~/LZ77/index.aux >> index.aux
+grep citation ~/LZ78/index.aux >> index.aux
 echo "\bibdata{text-compression}" >> index.aux
 echo "\bibstyle{plain}" >> index.aux
 #pdflatex citations
