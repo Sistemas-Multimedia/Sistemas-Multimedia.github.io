94194cae92bb1cf7ac4318bcdbd17389942d7b08
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-12-30 23:51:41 +0100

    .

--- runme.sh
+++ runme.sh
@@ -8,6 +8,7 @@ grep citation ~/LZW/index.aux >> index.aux
 grep citation ~/Shannon-Fano_coding/index.aux >> index.aux
 grep citation ~/Huffman_coding/index.aux >> index.aux
 grep citation ~/Arithmetic_coding/index.aux >> index.aux
+grep citation ~/move-to-front_transform/index.aux >> index.aux
 echo "\bibdata{text-compression}" >> index.aux
 echo "\bibstyle{plain}" >> index.aux
 #pdflatex citations
