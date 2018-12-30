3418f5aac0579899ab6790396d48abc33814ebd1
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-12-30 15:35:36 +0100

    .

--- runme.sh
+++ runme.sh
@@ -4,6 +4,10 @@ grep citation ~/Run-length_encoding/index.aux >> index.aux
 grep citation ~/Burrows-Wheeler_transform/index.aux >> index.aux
 grep citation ~/LZ77/index.aux >> index.aux
 grep citation ~/LZ78/index.aux >> index.aux
+grep citation ~/LZW/index.aux >> index.aux
+grep citation ~/Shannon-Fano_coding/index.aux >> index.aux
+grep citation ~/Huffman_coding/index.aux >> index.aux
+grep citation ~/Arithmetic_coding/index.aux >> index.aux
 echo "\bibdata{text-compression}" >> index.aux
 echo "\bibstyle{plain}" >> index.aux
 #pdflatex citations
