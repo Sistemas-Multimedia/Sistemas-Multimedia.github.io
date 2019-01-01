36673501ad5dcc9288b84a4c733ba662bf5f4ba3
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-01-01 12:41:47 +0100

    .

--- runme.sh
+++ runme.sh
@@ -13,7 +13,18 @@ grep citation ~/move-to-front_transform/index.aux >> index.aux
 grep citation ~/Golomb-Rice_coding/index.aux >> index.aux
 grep citation ~/gzip/index.aux >> index.aux
 grep citation ~/audio_compression/index.aux >> index.aux
+grep citation ~/quantization/index.aux >> index.aux
 grep citation ~/differential_coding/index.aux >> index.aux
+grep citation ~/MP3/index.aux >> index.aux
+grep citation ~/Vorbis/index.aux >> index.aux
+grep citation ~/image_compression/index.aux >> index.aux
+grep citation ~/image_redundancies/index.aux >> index.aux
+grep citation ~/image_transformations_for_coding/index.aux >> index.aux
+grep citation ~/LOCO/index.aux >> index.aux
+grep citation ~/JPEG/index.aux >> index.aux
+grep citation ~/JPEG2000/index.aux >> index.aux
+grep citation ~/MCTF-video-coding/index.aux >> index.aux
+grep citation ~/MCDWT/index.aux >> index.aux
 echo "\bibdata{signal-processing,text-compression,audio-coding}" >> index.aux
 echo "\bibstyle{plain}" >> index.aux
 #pdflatex citations
