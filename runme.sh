f26b078a7f3ea42e0f69524846244d81b2fb196f
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-05-13 13:22:54 +0200

    .

--- runme.sh
+++ runme.sh
@@ -1,14 +1,14 @@
 BIBINPUTS=":.:/home/vruiz/bibs/:"
 make clean
 grep citation ~/text_compression/index.aux >> index.aux
-grep citation ~/Run-length_encoding/index.aux >> index.aux
+grep citation ~/run-length_encoding/index.aux >> index.aux
 grep citation ~/Burrows-Wheeler_transform/index.aux >> index.aux
 grep citation ~/LZ77/index.aux >> index.aux
 grep citation ~/LZ78/index.aux >> index.aux
 grep citation ~/LZW/index.aux >> index.aux
 grep citation ~/Shannon-Fano_coding/index.aux >> index.aux
 grep citation ~/Huffman_coding/index.aux >> index.aux
-grep citation ~/Arithmetic_coding/index.aux >> index.aux
+grep citation ~/arithmetic_coding/index.aux >> index.aux
 grep citation ~/move-to-front_transform/index.aux >> index.aux
 grep citation ~/Golomb-Rice_coding/index.aux >> index.aux
 grep citation ~/gzip/index.aux >> index.aux
