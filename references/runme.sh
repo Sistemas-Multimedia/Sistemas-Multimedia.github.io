b3b7af86ec934469516a520e1c06a15d4e3f53a3
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-12-07 11:57:32 +0100

    .

--- references/runme.sh
+++ references/runme.sh
@@ -5,15 +5,16 @@ rm -f citations
 grep citation ~/Sistemas-Multimedia.github.io/milestones/01-provisioning/index.aux >> citations
 grep citation ~/Sistemas-Multimedia.github.io/milestones/02-python/index.aux >> citations
 grep citation ~/Sistemas-Multimedia.github.io/milestones/03-git/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/04-the_data/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/05-quantization/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/06-color_transform/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/07-DWT/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/08-2D-DWT/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/09-MDWT/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/04-PNG/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/05-RGB_compression/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/06-YUV_compression/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/07-DCT/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/08-DWT/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/09-III_coding/index.aux >> citations
 grep citation ~/Sistemas-Multimedia.github.io/milestones/10-ME/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/11-MC_in_DWT_domain/index.aux >> citations
-grep citation ~/Sistemas-Multimedia.github.io/milestones/12-IPP_coding/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/11-image_domain_IPP/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/12-transform_domain_MC/index.aux >> citations
+grep citation ~/Sistemas-Multimedia.github.io/milestones/13-transform_domain_IPP/index.aux >> citations
 cat citations >> index.aux
 bibtex index
 make index.html
